1f87c4a9cb3bc39e3927d1868e906f4838542935
xiaoli <lxy319@88.com>
2021-05-31 16:52:30 +0800

    添加了union文件夹

---
+++ union/Application/Admin/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //配置静态资源路径
+    'TMPL_PARSE_STRING' => array(
+        '__JS__'        => __ROOT__.'/Public/Admin/js',
+        '__CSS__'       => __ROOT__.'/Public/Admin/css',
+        '__IMG__'       => __ROOT__.'/Public/Admin/img',
+        '__FONT__'      => __ROOT__.'/Public/Admin/fonts',
+        '__PLUGINS__'   => __ROOT__.'/Public/Admin/plugins'
+    ),
+);
